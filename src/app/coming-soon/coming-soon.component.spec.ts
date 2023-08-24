7449c4970964d14839bb71598416baec640add80
ElliottDG <elliottdgregory@gmail.com>
2023-08-24 17:27:58 +0100

    closes #48

--- src/app/coming-soon/coming-soon.component.spec.ts
+++ src/app/coming-soon/coming-soon.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
 import { ComingSoonComponent } from './coming-soon.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatCardModule } from '@angular/material/card'
 
 describe('ComingSoonComponent', () => {
   let component: ComingSoonComponent;
@@ -10,7 +11,7 @@ describe('ComingSoonComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ComingSoonComponent],
-      imports: [MatIconModule, HttpClientTestingModule],
+      imports: [MatIconModule, HttpClientTestingModule, MatCardModule],
     });
     fixture = TestBed.createComponent(ComingSoonComponent);
     component = fixture.componentInstance;
